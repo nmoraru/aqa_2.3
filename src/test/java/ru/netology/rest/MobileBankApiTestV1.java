b1a43971abef4bb03716d2e69202b3f750c4678c
nikita moraru <test@mail.com>
2020-06-21 20:20:31 +0300

    upd tests

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -2,42 +2,25 @@ package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
 
+
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV1 {
+    File file = new File("/Users/Nikita Moraru/Desktop/Java_netology/aqa_2.3/src/test/resources/testData.json");
+
     @Test
     void shouldReturnDemoAccounts() {
       given()
           .baseUri("https://postman-echo.com")
-          .body("[\n" +
-                  "  {\n" +
-                  "    \"id\": 1,\n" +
-                  "    \"name\": \"Текущий счёт\",\n" +
-                  "    \"number\": \"•• 0668\",\n" +
-                  "    \"balance\": 992821429,\n" +
-                  "    \"currency\": \"RUB\"\n" +
-                  "  },\n" +
-                  "  {\n" +
-                  "    \"id\": 2,\n" +
-                  "    \"name\": \"Текущий счёт\",\n" +
-                  "    \"number\": \"•• 9192\",\n" +
-                  "    \"balance\": 2877210,\n" +
-                  "    \"currency\": \"USD\"\n" +
-                  "  },\n" +
-                  "  {\n" +
-                  "    \"id\": 3,\n" +
-                  "    \"name\": \"Текущий зарплатный счёт\",\n" +
-                  "    \"number\": \"•• 5257\",\n" +
-                  "    \"balance\": 1204044,\n" +
-                  "    \"currency\": \"RUB\"\n" +
-                  "  }\n" +
-                  "]")
+          .body(file)
       .when()
           .post("/post")
       .then()
           .statusCode(200)
-          .body("[0].currency", equalTo("RUB"))
+          .body("data[0].balance.toString()", equalTo("992821429"))
       ;
     }
 }

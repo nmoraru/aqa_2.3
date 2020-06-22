8d484ac2fe7e71f49cbead6f4e8ec0bab114c903
nikita moraru <test@mail.com>
2020-06-22 10:24:38 +0300

    upd test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -9,7 +9,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV1 {
-    File file = new File("/Users/Nikita Moraru/Desktop/Java_netology/aqa_2.3/src/test/resources/testData.json");
+    File file = new File("src/test/resources/testData.json");
 
     @Test
     void shouldReturnDemoAccounts() {
@@ -20,7 +20,7 @@ class MobileBankApiTestV1 {
           .post("/post")
       .then()
           .statusCode(200)
-          .body("data[0].balance.toString()", equalTo("992821429"))
+          .body("data[0].balance", equalTo(992821429))
       ;
     }
 }
